b0f3c3d2c632fdf8080ecab9d9448bd8cb2f309f
keitamarieme <marieme.keita@master-developpement-logiciel>
2020-04-30 08:11:04 +0200

    feat: Creation of repository interfaces

---
+++ back/src/main/java/com/teamrocket/projetdevop/ivvqproject/repositories/ShoppingCartRepository.java
@@ -0,0 +1,7 @@
+package com.teamrocket.projetdevop.ivvqproject.repositories;
+
+import com.teamrocket.projetdevop.ivvqproject.domain.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
+}
---
+++ back/src/main/java/com/teamrocket/projetdevop/ivvqproject/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.teamrocket.projetdevop.ivvqproject.repositories;
+
+import com.teamrocket.projetdevop.ivvqproject.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
