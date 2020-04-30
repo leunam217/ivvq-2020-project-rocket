d7f430ce8a309f78d2303e0d425f63e0261c744f
keitamarieme <marieme.keita@master-developpement-logiciel>
2020-04-30 08:41:40 +0200

    fix: Fixing Jpa interface extends fault on repository 'Integer -> Long'

--- back/src/main/java/com/teamrocket/projetdevop/ivvqproject/repositories/ProductRepository.java
+++ back/src/main/java/com/teamrocket/projetdevop/ivvqproject/repositories/ProductRepository.java
@@ -1,7 +1,8 @@
 package com.teamrocket.projetdevop.ivvqproject.repositories;
 
+import com.teamrocket.projetdevop.ivvqproject.domain.Product;
 import com.teamrocket.projetdevop.ivvqproject.domain.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<ShoppingCart, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
