05f4c70cd66597403ae0ced4fe80342553315da7
randriamalala <randriamalal@gmail.com>
2020-05-12 14:46:06 +0200

    Fix DATABASE_URL for production use

--- back/src/main/java/com/teamrocket/projetdevop/ivvqproject/DatabaseConfig.java
+++ back/src/main/java/com/teamrocket/projetdevop/ivvqproject/DatabaseConfig.java
@@ -13,14 +13,10 @@ public class DatabaseConfig {
     @Bean
     public DataSource dataSource() throws URISyntaxException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
-        /*URI dbUri = new URI("postgres://trbbmncqbzuirq" +
-                ":f9ab0cb034653f0eaa15052cb11347d040521f0665fb67eb7b02426470b74563@ec2-54-195-247-108.eu-west-1.compute.amazonaws.com:5432/d5nq4qb8hvrt7g");
-        */
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?createDatabaseIfNotExist=true";
-
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
