453b3227813e1f31e19337ae532a5686b034a8a5
randriamalala <randriamalal@gmail.com>
2020-05-12 15:37:04 +0200

    Fix heroku binding port

--- back/src/main/java/com/teamrocket/projetdevop/ivvqproject/controller/HelloController.java
+++ back/src/main/java/com/teamrocket/projetdevop/ivvqproject/controller/HelloController.java
@@ -3,12 +3,21 @@ package com.teamrocket.projetdevop.ivvqproject.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        try {
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+            String testPort = "Hello" + dbUri.toString();
+            return testPort;
+        } catch (URISyntaxException exception) {
+            return "Greetings from Spring Boot!";
+        }
     }
 
 }
