78ce7b472f79aa345a41f855b5a6aeb26148d6dc
Manuel CABARCOS BAULINA <crac.master@hotmail.com>
2020-05-15 11:23:24 +0200

    feat : add product template

--- front/ivvq-project/src/components/Product.vue
+++ front/ivvq-project/src/components/Product.vue
@@ -1,4 +1,4 @@
-<!--<template>
+<template>
   <v-card
     class="mx-auto"
     max-width="344"
@@ -9,21 +9,43 @@
     ></v-img>
 
     <v-card-title>
-        {{ name}}
+      {{ name}}
     </v-card-title>
 
     <v-card-subtitle>
-      1,000 miles of wonder
+      1 000 000 000 €
     </v-card-subtitle>
 
     <v-card-actions>
-      <v-btn text>Share</v-btn>
 
       <v-btn
-        color="purple"
-        text
+        class="mx-2"
+        fab
+        small
+        dark
+        color="orange"
       >
-        Explore
+        <v-icon dark>mdi-minus</v-icon>
+      </v-btn>
+      <span>{{quantity}}</span>
+      <v-btn
+        class="mx-2"
+        fab
+        small
+        dark
+        color="orange"
+      >
+        <v-icon dark>mdi-plus</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn
+        class="mx-2"
+        fab
+        small
+        dark
+        color="orange"
+      >
+        <v-icon dark>mdi-cart-plus</v-icon>
       </v-btn>
 
       <v-spacer></v-spacer>
@@ -39,7 +61,6 @@
     <v-expand-transition>
       <div v-show="show">
         <v-divider></v-divider>
-
         <v-card-text>
           I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
         </v-card-text>
@@ -54,108 +75,9 @@ import { Component, Vue, Prop } from "vue-property-decorator";
 @Component
 export default class Product extends Vue {
   show = false;
+  quantity = 0;
+  name = "Rocket";
   @Prop(String) readonly imageUrl: string =
     "https://img.pixers.pics/pho_wat(s3:700/FO/43/38/99/86/700_FO43389986_e1a030f717849f99319d1dec9c53186b.jpg,700,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/posters-flying-metal-rocket-isoated-vector-icon-3d.jpg.jpg";
 }
 </script>
--->
-<template>
-  <v-container>
-    <h2 class="display-2 mb-4">Basket</h2>
-
-    <v-list two-line>
-      <template v-for="(product, index) in products">
-        <v-list-tile
-          :key="product.productId"
-          avatar
-        >
-          <v-list-tile-avatar>
-            <img :src="product.photo">
-          </v-list-tile-avatar>
-
-          <v-list-tile-content>
-            <v-list-tile-title v-html="product.title"></v-list-tile-title>
-            <v-list-tile-sub-title v-html="product.subTitle"></v-list-tile-sub-title>
-          </v-list-tile-content>
-
-          <v-list-tile>
-            {{product.price}}$
-          </v-list-tile>
-
-          <v-list-tile-action>
-            <v-text-field
-              label="Quantity"
-              reverse
-              :value="product.qty"
-            ></v-text-field>
-          </v-list-tile-action>
-
-          <v-list-tile>
-            {{product.price * product.qty}}$
-          </v-list-tile>
-
-          <v-list-tile-action>
-            <v-btn
-              icon
-              ripple
-            >
-              <v-icon color="red lighten-1">delete</v-icon>
-            </v-btn>
-          </v-list-tile-action>
-        </v-list-tile>
-
-        <v-divider
-          v-if="index + 1 < products.length"
-          :key="index"
-        ></v-divider>
-      </template>
-    </v-list>
-
-    <v-container>
-      <v-btn
-        color="success"
-        larger
-        style="float: right;"
-      >Go to payment</v-btn>
-    </v-container>
-  </v-container>
-</template>
-
-<script>
-export default {
-  data: () => ({
-    products: [
-      {
-        productId: "kitchen-1",
-        photo: "https://cdn.vuetifyjs.com/images/cards/kitchen.png",
-        title: "QW cooking utensils",
-        subTitle:
-          "Our vintage kitchen utenils delight any chef. Made of bamboo by hand",
-        price: 14.99,
-        qty: 1
-      },
-      {
-        productId: "kitchen-1",
-        photo: "https://cdn.vuetifyjs.com/images/cards/kitchen.png",
-        title: "QW cooking utensils",
-        subTitle:
-          "Our vintage kitchen utenils delight any chef. Made of bamboo by hand",
-        price: 44.99,
-        qty: 2
-      },
-      {
-        productId: "kitchen-1",
-        photo: "https://cdn.vuetifyjs.com/images/cards/kitchen.png",
-        title: "QW cooking utensils",
-        subTitle:
-          "Our vintage kitchen utenils delight any chef. Made of bamboo by hand",
-        price: 64.99,
-        qty: 2
-      }
-    ]
-  })
-};
-</script>
-
-<style scoped>
-</style>
--- front/ivvq-project/src/views/Home.vue
+++ front/ivvq-project/src/views/Home.vue
@@ -1,14 +1,56 @@
 <template>
-  <div class="home">
+  <div class="d-flex flex-column justify-space-between fill-height">
+    <Toolbar> </Toolbar>
+    <v-container class="align-self-center">
+      <v-row>
+        <v-col>
+          <Product></Product>
+        </v-col>
+        <v-col>
+          <Product></Product>
+        </v-col>
+        <v-col>
+          <Product></Product>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <Product></Product>
+        </v-col>
+        <v-col>
+          <Product></Product>
+        </v-col>
+        <v-col>
+          <Product></Product>
+        </v-col>
+      </v-row>
+      <v-row justify="start">
+        <v-col>
+          <Product></Product>
+        </v-col>
+        <v-col>
+          <Product></Product>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Product from "@/components/Product";
+import Toolbar from "@/components/Toolbar";
+import { Component, Vue } from "vue-property-decorator";
 
-export default {
-  name: 'Home',
+@Component({
   components: {
+    Toolbar,
+    Product
   }
-}
+})
+export default class Login extends Vue {}
 </script>
